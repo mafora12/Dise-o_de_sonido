7ba9ecdc231c22fcea6a29492e511a33b9bd9811
mafora12 <marianaflorez1224@gmail.com>
2026-05-24 12:15:27 -0500

    adelanto

--- disñeo_sonoro final/Assets/Karting/Scripts/KartSystems/KartAudio/ArcadeEngineAudio.cs
+++ disñeo_sonoro final/Assets/Karting/Scripts/KartSystems/KartAudio/ArcadeEngineAudio.cs
@@ -8,33 +8,42 @@ namespace KartGame.KartSystems
         public float minRPM = 500;
         public float maxRPM = 2000;
         ArcadeKart arcadeKart;
-        private StudioEventEmitter[] emitters;
-        private StudioEventEmitter motorEmitter;
-        private StudioEventEmitter reverseEmitter;
-        private StudioEventEmitter driftEmitter;
+
+        [Header("FMOD Events")]
+        public StudioEventEmitter idleEmitter;
+        public StudioEventEmitter runningEmitter;
+        public StudioEventEmitter reverseEmitter;
+        public StudioEventEmitter driftEmitter;
 
         void Awake()
         {
             arcadeKart = GetComponentInParent<ArcadeKart>();
-            emitters = GetComponents<StudioEventEmitter>();
-            foreach (var e in emitters)
-            {
-                if (e.EventReference.Path.Contains("mi_motor")) motorEmitter = e;
-                if (e.EventReference.Path.Contains("engine_reverse")) reverseEmitter = e;
-                if (e.EventReference.Path.Contains("drift")) driftEmitter = e;
-            }
         }
 
         void Update()
         {
             float kartSpeed = arcadeKart != null ? arcadeKart.LocalSpeed() : 0;
-            float effectiveRPM = Mathf.Lerp(minRPM, maxRPM, Mathf.Abs(kartSpeed));
 
-            if (motorEmitter != null)
-                motorEmitter.SetParameter("RPM", effectiveRPM);
-
-            if (reverseEmitter != null)
-                reverseEmitter.SetParameter("RPM", kartSpeed < 0 ? effectiveRPM : 0);
+            if (kartSpeed < 0)
+            {
+                idleEmitter?.Stop();
+                runningEmitter?.Stop();
+                reverseEmitter?.Play();
+            }
+            else
+            {
+                reverseEmitter?.Stop();
+                if (kartSpeed < 0.1f)
+                {
+                    runningEmitter?.Stop();
+                    idleEmitter?.Play();
+                }
+                else
+                {
+                    idleEmitter?.Stop();
+                    runningEmitter?.Play();
+                }
+            }
 
             if (driftEmitter != null && arcadeKart != null)
             {
